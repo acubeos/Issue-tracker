c3b5fee1216e9d8453e728cbad5dd34383811ead
acubeos <80309463+acubeos@users.noreply.github.com>
2023-12-24 12:26:40 +0100

    Build an API for deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -29,3 +29,23 @@ export async function PATCH(
 
 	return NextResponse.json(updatedIssue)
 }
+
+export async function DELETE(
+	request: NextRequest,
+	{ params: { id } }: { params: { id: string } }
+) {
+	const issue = await prisma.issue.findUnique({
+		where: { id: parseInt(id) },
+	})
+
+	if (!issue)
+		return NextResponse.json({ error: "Invalid issue" }, { status: 404 })
+
+	await prisma.issue.delete({
+		where: {
+			id: parseInt(id),
+		},
+	})
+
+	return NextResponse.json({})
+}
