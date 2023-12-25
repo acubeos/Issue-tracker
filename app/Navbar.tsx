5e9068ad18bc5b264426d88590475d156e05a2e5
acubeos <80309463+acubeos@users.noreply.github.com>
2023-12-25 17:29:19 +0100

    Add the signin and signout links

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -4,6 +4,8 @@ import Link from "next/link"
 import { usePathname } from "next/navigation"
 import React from "react"
 import { FaBug } from "react-icons/fa"
+import { useSession } from "next-auth/react"
+import { Box } from "@radix-ui/themes"
 
 const Navbar = () => {
 	const links = [
@@ -11,6 +13,7 @@ const Navbar = () => {
 		{ label: "Issues", href: "/issues/list" },
 	]
 
+	const { status, data: session } = useSession()
 	const pathName = usePathname()
 	return (
 		<nav className='flex space-x-6 h-16 items-center border-b mb-6 px-4'>
@@ -20,7 +23,7 @@ const Navbar = () => {
 
 			<ul className='flex space-x-6'>
 				{links.map((link) => (
-					<li key={link.label}>
+					<li key={link.href}>
 						<Link
 							className={classnames({
 								"text-zinc-900": link.href === pathName,
@@ -34,6 +37,14 @@ const Navbar = () => {
 					</li>
 				))}
 			</ul>
+			<Box>
+				{status === "authenticated" && (
+					<Link href='/api/auth/signout'>Sign out</Link>
+				)}
+				{status === "unauthenticated" && (
+					<Link href='/api/auth/signin'>Sign in</Link>
+				)}
+			</Box>
 		</nav>
 	)
 }
