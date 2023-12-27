da3d7b4f3e0b103448e091f120fdc2c88c4e8c12
acubeos <80309463+acubeos@users.noreply.github.com>
2023-12-27 17:43:28 +0100

    Enhance API to assigned issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,4 +1,4 @@
-import { issueSchema } from "@/app/validationSchema"
+import { patchIssueSchema } from "@/app/validationSchema"
 import { NextRequest, NextResponse } from "next/server"
 import prisma from "@/prisma/client"
 import authOptions from "@/app/auth/authOptions"
@@ -13,9 +13,18 @@ export async function PATCH(
 
 	const body = await request.json()
 
-	const validation = issueSchema.safeParse(body)
+	const validation = patchIssueSchema.safeParse(body)
 	if (!validation.success)
 		return NextResponse.json(validation.error.format(), { status: 400 })
+
+	const { assignedToUserId, title, description } = body
+	if (assignedToUserId) {
+		const user = await prisma.user.findUnique({
+			where: { id: assignedToUserId },
+		})
+		if (!user)
+			return NextResponse.json({ error: "Invalid User" }, { status: 400 })
+	}
 
 	const issue = await prisma.issue.findUnique({
 		where: { id: parseInt(id) },
@@ -27,8 +36,9 @@ export async function PATCH(
 	const updatedIssue = await prisma.issue.update({
 		where: { id: issue.id },
 		data: {
-			title: body.title,
-			description: body.description,
+			title,
+			description,
+			assignedToUserId,
 		},
 	})
 
--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -4,6 +4,21 @@ export const issueSchema = z.object({
 	title: z
 		.string()
 		.min(3, { message: "Title must contain at least 3 characters" })
-		.max(250, { message: "Title must not exceed 250 characters" }),
-	description: z.string().min(1),
+		.max(255, { message: "Title must not exceed 250 characters" }),
+	description: z.string().min(1).max(65535),
+})
+
+export const patchIssueSchema = z.object({
+	title: z
+		.string()
+		.min(3, { message: "Title must contain at least 3 characters" })
+		.max(255, { message: "Title must not exceed 250 characters" })
+		.optional(),
+	description: z.string().min(1).max(65535).optional(),
+	assignedToUserId: z
+		.string()
+		.min(1, { message: "AssignedToUserId is required" })
+		.max(255, { message: "AssignedToUserId must not exceed 250 characters" })
+		.optional()
+		.nullable(),
 })
