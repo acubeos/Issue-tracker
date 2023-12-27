de503fee912b0bca72e431d1eff4c5680e7366ac
acubeos <80309463+acubeos@users.noreply.github.com>
2023-12-27 15:30:52 +0100

    Build assignee select component

---
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -0,0 +1,19 @@
+"use client"
+import { Select } from "@radix-ui/themes"
+import React from "react"
+
+const AssigneeSelect = () => {
+	return (
+		<Select.Root>
+			<Select.Trigger placeholder='assign...' />
+			<Select.Content>
+				<Select.Group>
+					<Select.Label>Suggestions</Select.Label>
+					<Select.Item value='1'>Abiola</Select.Item>
+				</Select.Group>
+			</Select.Content>
+		</Select.Root>
+	)
+}
+
+export default AssigneeSelect
