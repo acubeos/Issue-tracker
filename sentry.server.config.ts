3b85c865ebb0ed218748b337a3540eb2dc776ab5
acubeos <80309463+acubeos@users.noreply.github.com>
2024-01-01 22:24:46 +0100

    Set up error tracking

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://92c0cb88dc9f3db60ea7ad94e0ab7907@o4506498023817216.ingest.sentry.io/4506498029387776",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
