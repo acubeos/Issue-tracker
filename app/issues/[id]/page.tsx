ead8de449f972ed00ecf11e376bf5986d14d0b73
acubeos <80309463+acubeos@users.noreply.github.com>
2024-01-01 20:00:05 +0100

    Optimize performance using react cache in issue detail page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -7,16 +7,20 @@ import AssigneeSelect from "./AssigneeSelect"
 import DeleteIssueButton from "./DeleteIssueButton"
 import EditIssueButton from "./EditIssueButton"
 import IssueDetails from "./IssueDetails"
+import { cache } from "react"
 
 interface Props {
 	params: { id: string }
 }
 
+const fetchUser = cache((issueId: number) =>
+	prisma.issue.findUnique({ where: { id: issueId } })
+)
+
 const page = async ({ params: { id } }: Props) => {
 	const session = await getServerSession(authOptions)
-	const issue = await prisma.issue.findUnique({
-		where: { id: parseInt(id) },
-	})
+
+	const issue = await fetchUser(parseInt(id))
 
 	if (!issue) notFound()
 
@@ -39,9 +43,7 @@ const page = async ({ params: { id } }: Props) => {
 }
 
 export async function generateMetadata({ params }: Props) {
-	const issue = await prisma.issue.findUnique({
-		where: { id: parseInt(params.id) },
-	})
+	const issue = await fetchUser(parseInt(params.id))
 	return {
 		title: issue?.title,
 		description: `Details of issue ${issue?.id}`,
